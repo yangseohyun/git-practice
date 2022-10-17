5f3dc34dc78eb6e329daeb3cd8710d249602d5ae
Seohyun Yang <qk325672@gachon.ac.kr>
2022-10-17 23:39:55 +0900

    initial commit

---
+++ backup_module.py
@@ -0,0 +1,5 @@
+total 3.0K
+-rw-r--r-- 1 USER 197121 108 Oct 17 22:47 README.md
+-rw-r--r-- 1 USER 197121 181 Oct 17 22:49 classification_experiment.py
+-rw-r--r-- 1 USER 197121  53 Oct 17 22:47 file
+-rw-r--r-- 1 USER 197121   0 Oct 17 22:49 module.py
---
+++ test.sh
@@ -0,0 +1,7 @@
+total 5.0K
+-rw-r--r-- 1 USER 197121 108 Oct 17 22:47 README.md
+-rw-r--r-- 1 USER 197121 233 Oct 17 22:55 backup_module.py
+-rw-r--r-- 1 USER 197121 181 Oct 17 22:49 classification_experiment.py
+-rw-r--r-- 1 USER 197121  53 Oct 17 22:47 file
+-rw-r--r-- 1 USER 197121 294 Oct 17 22:59 hello_world
+-rw-r--r-- 1 USER 197121   0 Oct 17 22:59 test.sh
